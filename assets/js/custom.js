23467993b002c71e32a62ea6e46a749c312471dc
Kongnuy <kongnuyvictorien@yahoo.com>
2019-01-02 15:13:50 +0100

    commit 8

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -84,5 +84,20 @@ jQuery(document).ready(function ($) {
             'copy', 'csv', 'excel', 'pdf', 'print'
         ]
       });
+
+    $('.dropdown-toggle').click(function(e) {
+        if ($(document).width() > 768) {
+            e.preventDefault();
+
+            var url = $(this).attr('href');
+
+            if (url !== '#') {
+            
+            window.location.href = url;
+            }
+
+        }
     });
+
+});
   
